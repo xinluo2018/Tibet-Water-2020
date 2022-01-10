6913a677fbdf2bdca1b0ac6f1e247d841d484f15
xin luo <xinluo_xin@163.com>
2022-01-10 15:14:29 +0800

    new commit

--- tibet_water_run.sh
+++ tibet_water_run.sh
@@ -10,7 +10,7 @@ des_path=/myDrive/tibet-water/tibet-202012/s1_descend/*.tif
 model=/home/yons/Desktop/developer-luo/Monthly-Surface-Water-in-Tibet/model/pretrained/apply_to_tibet/model_gscales_app_base_weights.pth
 model_as=/home/yons/Desktop/developer-luo/Monthly-Surface-Water-in-Tibet/model/pretrained/apply_to_tibet/model_gscales_as_app_base_weights.pth
 model_des=/home/yons/Desktop/developer-luo/Monthly-Surface-Water-in-Tibet/model/pretrained/apply_to_tibet/model_gscales_des_app_base_weights.pth
-
+ 
 python swatnet_infer.py -as $as_path_1 -des $des_path -s 100 -m $model -m_as $model_as -m_des $model_des
 python swatnet_infer.py -as $as_path_2 -des $des_path -s 100 -m $model -m_as $model_as -m_des $model_des
 python swatnet_infer.py -as $as_path_3 -des $des_path -s 100 -m $model -m_as $model_as -m_des $model_des
